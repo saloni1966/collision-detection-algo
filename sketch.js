2b7a730b1d92d58f96e6e7d229777720e2fe0309
saloni1966 <salonisharma1966@gmail.com>
2020-11-24 19:27:24 +0530

    first commit

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var fixedRect,movingRect;
+
+function setup() {
+  createCanvas(800,400);
+  fixedRect = createSprite(400, 200, 50, 50);
+  fixedRect.shapeColor = "blue";
+  movingRect = createSprite(500,200,50,90);
+  movingRect.shapeColor = "green";
+}
+
+function draw() {
+  background(0);  
+  movingRect.x = mouseX;
+  movingRect.y = mouseY;
+
+  console.log(movingRect.x - fixedRect.x );
+  console.log(movingRect.width/2 + fixedRect.width/2);
+  if(movingRect.x - fixedRect.x <  movingRect.width/2 + fixedRect.width/2 && 
+    fixedRect.x - movingRect.x < movingRect.width/2 + fixedRect.width/2){
+    fixedRect.shapeColor = "red";
+    movingRect.shapeColor = "red";
+}
+  else {
+    fixedRect.shapeColor = "blue";
+    movingRect.shapeColor = "green";
+  }
+
+  drawSprites();
+}
